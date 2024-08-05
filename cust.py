3f5a49f4dd3109c83c8d7409425b5aa7a251626c
packshark <sb016n@att.com>
2024-08-05 22:57:32 +0000

    initial test and set up of cust page

--- cust.py
+++ cust.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.write("# Customer")
+st.title("Customer Portal")
+
+# sample table until we have the data
+table = pd.DataFrame({"User": ["Stephanie","Jeremy","Nathaniel","Christiana","Rithik"],
+                      "Device": ["Mobile","Desktop","Tablet","Watch","TV"],
+                      "Blocked_Domains": ["https://blahblah.com","hahahahah.com","https://fkshdfkjsfhks.com","https://fafsa.gov","bing.com"],
+                      "Count":[20,5,2,4,5]})
+st.table(table)
