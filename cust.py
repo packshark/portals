c509af9d5702e6a650159532c48ed7f611783186
packshark <sb016n@att.com>
2024-08-05 17:05:10 -0700

    Update cust.py - add navigation

--- cust.py
+++ cust.py
@@ -2,10 +2,16 @@ import streamlit as st
 import pandas as pd
 
 st.title("Customer Portal")
+st.markdown("Customer Portal")
+st.sidebar.header("Customer Portal")
 
+# add filters
 # sample table until we have the data
 table = pd.DataFrame({"User": ["Stephanie","Jeremy","Nathaniel","Christiana","Rithik"],
                       "Device": ["Mobile","Desktop","Tablet","Watch","TV"],
                       "Blocked_Domains": ["https://blahblah.com","hahahahah.com","https://fkshdfkjsfhks.com","https://fafsa.gov","bing.com"],
                       "Count":[20,5,2,4,5]})
 st.table(table)
+
+# Power BI table
+# do we want to upload and reflect anything?
