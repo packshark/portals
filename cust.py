f34331e586789b663250ee13c331d2d7ff6d534f
packshark <sb016n@att.com>
2024-08-12 16:32:01 -0700

    view table with data frame

--- cust.py
+++ cust.py
@@ -8,7 +8,7 @@ st.sidebar.header("Customer Portal")
 st.sidebar.header("Operations Portal")
 
 # table Rithik shared -- though we might not need this once we have the Power BI table and can do everything through there
-table = pd.read("https://github.com/packshark/portals/blob/main/fake_dns_log.csv")
+table = pd.DataFrame(pd.read_csv("https://github.com/packshark/portals/blob/main/fake_dns_log.csv"))
 st.table(table)
 
 # Power BI table
