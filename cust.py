31dfe52187a829db514eb7da87ccd670b2634770
packshark <sb016n@att.com>
2024-08-12 15:46:44 -0700

    iframe code attempt

--- cust.py
+++ cust.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import streamlit.components.v1 as components
 
 st.title("Customer Portal")
 st.markdown("Customer Portal")
@@ -16,4 +17,5 @@ st.table(table)
 
 # Power BI table
 # do we want to upload and reflect anything?
+components.iframe("https://app.powerbi.com/reportEmbed?reportId=c2d0d27d-4aab-4cb8-94ab-f793dade383c&autoAuth=true&ctid=e741d71c-c6b6-47b0-803c-0f3b32b07556", height =500)
 # <iframe title="nathaniel's non-user error report" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=c2d0d27d-4aab-4cb8-94ab-f793dade383c&autoAuth=true&ctid=e741d71c-c6b6-47b0-803c-0f3b32b07556" frameborder="0" allowFullScreen="true"></iframe>
