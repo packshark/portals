ee156f8cfe1d7cf5d3c6a6e1b70a0f97cca20a57
packshark <sb016n@att.com>
2024-08-13 10:22:05 -0700

    Update workingcopy.py
    pasted my short working code

--- workingcopy.py
+++ workingcopy.py
@@ -1,1 +1,19 @@
 # making a copy here to avoid making apps for other pages, will test login code on cust.py so my working code is here
+
+import streamlit as st
+import pandas as pd
+import streamlit.components.v1 as components
+
+st.title("Customer Portal")
+st.markdown("Customer Portal")
+st.sidebar.header("Customer Portal")
+st.sidebar.header("Operations Portal")
+
+# table Rithik shared -- though we might not need this once we have the Power BI table and can do everything through there
+data = pd.read_csv("fake_dns_log.csv")
+st.dataframe(data)
+
+# Power BI table
+# do we want to upload and reflect anything?
+components.iframe("https://app.powerbi.com/reportEmbed?reportId=c2d0d27d-4aab-4cb8-94ab-f793dade383c&autoAuth=true&ctid=e741d71c-c6b6-47b0-803c-0f3b32b07556", height =600, width = 800)
+
