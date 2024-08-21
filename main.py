79f89141a197257c0f9ad3fc52d304f03b59fd8e
packshark <sb016n@att.com>
2024-08-21 14:54:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import streamlit as st
 import os
 import pathlib
 
+# Get the current path of the main file
+current_path = pathlib.Path(__file__)
+
+# Get the directory of the current path
+current_dir = os.path.dirname(current_path)
+
+# Change the destination URL to '/mount/src/portals/pages/cust.py'
+new_path = os.path.join(current_dir, 'pages', 'cust.py')
+st.write(current_path)
+st.write(current_dir)
+st.write(new_path)
+
 # Main content
 st.title("Welcome to the Packing Portal")
 st.write("Please log in to continue")
@@ -11,15 +23,6 @@ with st.form("login_form"):
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
     submit_button = st.form_submit_button('Submit')
-
-# Get the current path of the main file
-current_path = pathlib.Path(__file__)
-
-# Get the directory of the current path
-current_dir = os.path.dirname(current_path)
-
-# Change the destination URL to '/mount/src/portals/pages/cust.py'
-new_path = os.path.join(current_dir, 'pages', 'cust.py')
 
 if submit_button:
     if username == "phone" and password == "hellokitty":
