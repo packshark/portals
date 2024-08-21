80260ac433e59fb2074fabc37151bd3703bd0a0b
packshark <sb016n@att.com>
2024-08-21 09:46:42 -0700

    Update basicUser.py

--- basicUser.py
+++ basicUser.py
@@ -1,6 +1,10 @@
 # a user that does not have access to see everything
 import streamlit as st
 
+st.title("Uploading Files")
+st.subheader("You have reached this page due to your current access permissions")
+st.markdown("---")
+
 uploaded_files = st.file_uploader(
     "Choose a CSV file", accept_multiple_files=True
 )
@@ -9,3 +13,9 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
+
+# Can be used wherever a "file-like" object is accepted:
+dataframe = pd.read_csv(uploaded_file)
+st.write(dataframe)
+
+# later want to pass this to powerBI
