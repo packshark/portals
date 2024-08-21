9a1dcc2e7cf79d2834c1673cb0f87059c967a5bc
packshark <sb016n@att.com>
2024-08-21 11:56:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ with st.form("login_form"):
 
 if submit_button:
     if username == "phone" and password == "hellokitty":
-        st.switch_page("main/workingcopy.py")
+        st.switch_page("pages/cust.py")
     elif username == "laptop" and password == "chamberofsecrets":
-        st.switch_page("main/operations.py")
+        st.switch_page("pages/operations.py")
     elif username == "hehe" and password == "helloworld":
-        st.switch_page("main/basicUser.py")
+        st.switch_page("pages/basicUser.py")
     else:
         st.error("Incorrect username or password")
